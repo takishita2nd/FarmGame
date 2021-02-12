0b9de9f37665926179ec198811fc37785ed3d4c3
滝下 則幸 <n.taki.0416@gmail.com>
2021-02-13 06:15:14 +0900

    農場の設計

---
+++ FarmGame/Model/Farm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FarmGame.Model
+{
+    class Farm
+    {
+        List<FarmField> farmFields;
+    }
+
+    class FarmField
+    {
+        //植えているかどうか
+        public bool valid { get; set; }
+        //種ID
+        public int id { get; set; }
+        //水
+        public bool water { get; set; }
+        //成長度
+        public int growth { get; set; }
+        //品質
+        public int quality { get; set; }
+    }
+}
---
+++ FarmGame/Model/Plant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FarmGame.Model
+{
+    class Plant
+    {
+        //種ID
+        public int id { get; set;}
+
+        //名前
+        public string name { get; set; }
+
+        //育成コスト
+        public int cost { get; set; }
+
+        //種の代金
+        public int money { get; set; }
+    }
+}
